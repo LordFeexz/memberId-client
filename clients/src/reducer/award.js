8797640a3db37e08d4a013d2b34010f75a8847c6
LordFeexz <anandafiqri1403@gmail.com>
2023-01-06 22:43:38 +0700

    setup

---
+++ clients/src/reducer/award.js
@@ -0,0 +1,11 @@
+const initialState = { awards: [] };
+
+export const awardReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case value:
+      break;
+
+    default:
+      return state;
+  }
+};
